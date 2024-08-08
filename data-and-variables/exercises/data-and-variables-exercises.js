5af30420f679c0c796c6e9b300ee57934a15843d
ZerefRD <juanman.1305@gmail.com>
2024-08-08 03:12:22 -0400

    updates

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -4,6 +4,21 @@
 
 // Calculate a space mission below
 
+let x = 3
+let y = 5
+
+x = x+1
+y = y+2
+
+y= y+4
+
+
+
+
+console.log(2 ** 2 ** 3 * 3
+);
+
+
 // Print the results of the space mission calculations below
 
 // Calculate a trip to the moon below
--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,55 +1,104 @@
 // Code your selectRandomEntry function here:
 
+let idSelected = [];
+
+function selectRandomEntry(arr) {
+
+  while (idSelected.length < 3) {
+    let index = Math.floor(Math.random() * arr.length);
+    let = selectedItem = arr[index]
+    if (!idSelected.includes(selectedItem)) {
+      idSelected.push(selectedItem);
+
+    }
+  }
+    return idSelected;
+  
+}
+
 
 // Code your buildCrewArray function here:
 
+let keysFound = []
+function buildCrewArray (arr1, arr2){
+  let crew = []
+
+  for (let key in arr2){
+    keysFound.push(key)
+  }
+
+}
 
 let idNumbers = [291, 414, 503, 599, 796, 890];
 
+
 // Here are the candidates and the 'animals' array:
 let candidateA = {
-  'name':'Gordon Shumway',
-  'species':'alf',
-  'mass':90,
-  'o2Used':function(hrs){return 0.035*hrs},
-  'astronautID':414
-};
-let candidateB = {
-  'name':'Lassie',
-  'species':'dog',
-  'mass':19.1,
-  'o2Used':function(hrs){return 0.030*hrs},
-  'astronautID':503
-};
-let candidateC = {
-  'name':'Jonsey',
-  'species':'cat',
-  'mass':3.6,
-  'o2Used':function(hrs){return 0.022*hrs},
-  'astronautID':796
-};
-let candidateD = {
-  'name':'Paddington',
-  'species':'bear',
-  'mass':31.8,
-  'o2Used':function(hrs){return 0.047*hrs},
-  'astronautID':291
-};
-let candidateE = {
-  'name':'Pete',
-  'species':'tortoise',
-  'mass':417,
-  'o2Used':function(hrs){return 0.010*hrs},
-  'astronautID':599
-};
-let candidateF = {
-  'name':'Hugs',
-  'species':'ball python',
-  'mass':2.3,
-  'o2Used':function(hrs){return 0.018*hrs},
-  'astronautID':890
+  name: "Gordon Shumway",
+  species: "alf",
+  mass: 90,
+  o2Used: function (hrs) {
+    return 0.035 * hrs;
+  },
+  astronautID: 414,
 };
 
-let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
+console.log(buildCrewArray(idNumbers, candidateA));
+console.log(keysFound)
+
+let candidateB = {
+  name: "Lassie",
+  species: "dog",
+  mass: 19.1,
+  o2Used: function (hrs) {
+    return 0.03 * hrs;
+  },
+  astronautID: 503,
+};
+let candidateC = {
+  name: "Jonsey",
+  species: "cat",
+  mass: 3.6,
+  o2Used: function (hrs) {
+    return 0.022 * hrs;
+  },
+  astronautID: 796,
+};
+let candidateD = {
+  name: "Paddington",
+  species: "bear",
+  mass: 31.8,
+  o2Used: function (hrs) {
+    return 0.047 * hrs;
+  },
+  astronautID: 291,
+};
+let candidateE = {
+  name: "Pete",
+  species: "tortoise",
+  mass: 417,
+  o2Used: function (hrs) {
+    return 0.01 * hrs;
+  },
+  astronautID: 599,
+};
+let candidateF = {
+  name: "Hugs",
+  species: "ball python",
+  mass: 2.3,
+  o2Used: function (hrs) {
+    return 0.018 * hrs;
+  },
+  astronautID: 890,
+};
+
+let animals = [
+  candidateA,
+  candidateB,
+  candidateC,
+  candidateD,
+  candidateE,
+  candidateF,
+];
 
 // Code your template literal and console.log statements:
